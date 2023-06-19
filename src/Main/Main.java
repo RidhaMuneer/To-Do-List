5659abb43328bb969155d47f7046745cd353c9a8
Ridha Muneer <ridhamuner@gmail.com>
2023-06-19 17:25:27 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,6 @@
 package Main;
 
-//Ridha Muner Kamil(rm20347@auis.edu.krd)
-//Asaad Thiyab Issa(at20134@auis.edu.krd)
-
-//you can enter the url, username, and password of your local database at the DBConnection class in the Database package.
+//you can enter the URL, username, and password of your local database at the DBConnection class in the Database package.
 
 import java.sql.SQLException;
 
